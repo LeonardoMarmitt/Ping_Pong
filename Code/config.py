eb9d7afdce5c7c81703cbd2f4bf9807a9b60ab88
LeonardoMarmitt <leonardomarmitt@hotmail.com>
2025-07-04 21:17:04 +0800

    Innitial commit

---
+++ Code/config.py
@@ -0,0 +1,25 @@
+# Tamanho da janela
+LARGURA = 800
+ALTURA = 500
+
+# Frames por segundo
+FPS = 60
+
+# Cores RGB
+AZUL_MESA = (28, 107, 160)
+BRANCO = (255, 255, 255)
+VERDE = (0, 200, 0)
+VERMELHO = (200, 0, 0)
+
+# Tamanho da raquete
+RAQUETE_LARGURA = 20
+RAQUETE_ALTURA = 100
+
+# Raio da bola
+BOLA_RAIO = 12
+
+# Velocidades
+VEL_RAQUETE = 8
+VEL_BOLA_X = 6
+VEL_BOLA_Y = 6
+VEL_IA = 5
